4ae4edaf258d022eded1bb4d7571f60ba5285db8
cacalawak <risayusnita02@gmail.com>
2023-12-27 20:34:49 +0700

    membuat TopAnime.kt

---
+++ app/src/main/java/com/example/newproject/main/TopAnime.kt
@@ -0,0 +1,17 @@
+package com.example.newproject.main
+
+import kotlin.Result
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TopAnime(
+    @SerializedName("request_cache_expiry")
+    val requestCacheExpiry: Int,
+    @SerializedName("request_cached")
+    val requestCached: Boolean,
+    @SerializedName("request_hash")
+    val requestHash: String,
+    @SerializedName("data")
+    val top: List<Result>
+)
