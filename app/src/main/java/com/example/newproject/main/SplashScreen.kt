f003edb32bc34d820fcc9a0f98bf78bdc167f615
cacalawak <risayusnita02@gmail.com>
2023-12-27 20:33:53 +0700

    membuat SplashScreen.kt

---
+++ app/src/main/java/com/example/newproject/main/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.newproject.main
+
+import kotlin.Result
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchedAnime(
+    @SerializedName("last_page")
+    val lastPage: Int,
+    @SerializedName("request_cache_expiry")
+    val requestCacheExpiry: Int,
+    @SerializedName("request_cached")
+    val requestCached: Boolean,
+    @SerializedName("request_hash")
+    val requestHash: String,
+    @SerializedName("results")
+    val results: List<Result>
+)
